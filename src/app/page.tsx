6b1a5fbf189db157b8a2124b606bcafa8f7ac76d
SubodhKumarSahu2826 <code.sks26@gmail.com>
2025-05-11 16:57:35 +0530

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+import Index from "@/components/Dashboard";
+import { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+
+export const metadata: Metadata = {
+  title:
+    "Protien Bind: a leading research platform for drug research and discovery",
+  description: "This is a description for protein bind",
+};
+
 export default function Home() {
   return (
     <>
-      <div className="h-screen">
-        <p>Default Layout</p>
-      </div>
+      <DefaultLayout>
+        <Index />
+      </DefaultLayout>
     </>
   );
 }
